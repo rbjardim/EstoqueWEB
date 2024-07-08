69686c624ac4487ac6e77075962ac4d50a8d9a56
rbjardim <ruan.bueno70@gmail.com>
2024-07-08 13:46:05 -0300

    Ajuste em interface

--- EstoqueWEB/EstoqueWEB/obj/Debug/net8.0/EstoqueWEB.AssemblyInfo.cs
+++ EstoqueWEB/EstoqueWEB/obj/Debug/net8.0/EstoqueWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstoqueWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1f31312dee25c5ee53bca3a21684bbe21aa55c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d935796240ca9bff300758cf064ae244b34040ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstoqueWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstoqueWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
