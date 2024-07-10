8f448f7c070f065152a9a88b576eba8e16dba2c5
rznnakdavila <ruan.bueno70@gmail.com>
2024-07-09 23:42:38 -0300

    Ajuste em Edit page devolução

--- EstoqueWEB/EstoqueWEB/obj/Debug/net8.0/EstoqueWEB.AssemblyInfo.cs
+++ EstoqueWEB/EstoqueWEB/obj/Debug/net8.0/EstoqueWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstoqueWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3dbd8d01f85e0ca6959b45333c9d97a01341154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff67e7a4124eca88971a4240c91a02a574318e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstoqueWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstoqueWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
